92f6a53e73ef05d3795abfee56f2e951985cc133
manyajain <manyajain0212@gmail.com>
2025-04-21 14:13:47 +0530

    Added function for adding roles into Role table

--- bloggingApp/src/main/java/com/springboot/bloggingApp/BloggingAppApplication.java
+++ bloggingApp/src/main/java/com/springboot/bloggingApp/BloggingAppApplication.java
@@ -1,13 +1,49 @@
 package com.springboot.bloggingApp;
 
+import com.springboot.bloggingApp.config.AppConstant;
+import com.springboot.bloggingApp.entity.Role;
+import com.springboot.bloggingApp.repository.RoleRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class BloggingAppApplication {
+public class BloggingAppApplication  implements CommandLineRunner {
+
+	@Autowired
+	private RoleRepo roleRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BloggingAppApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		try {
+
+			Role role = new Role();
+			role.setId(AppConstant.ADMIN_USER);
+			role.setName("ROLE_ADMIN");
+
+			Role role1 = new Role();
+			role1.setId(AppConstant.NORMAL_USER);
+			role1.setName("ROLE_NORMAL");
+
+			List<Role> roles = List.of(role, role1);
+
+			List<Role> result = this.roleRepo.saveAll(roles);
+
+			result.forEach(r -> {
+				System.out.println(r.getName());
+			});
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
 }
